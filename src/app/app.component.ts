d2fc5e50ed57b4afb026e2c066e9dc73bdb407ff
GediminasGricius <88775405+GediminasGricius@users.noreply.github.com>
2024-05-09 10:55:10 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewEmpoloyeeComponent } from './components/employees/new-empoloyee/new-empoloyee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewEmpoloyeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/employees/new-empoloyee/new-empoloyee.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewEmpoloyeeComponent } from './new-empoloyee.component';
+
+describe('NewEmpoloyeeComponent', () => {
+  let component: NewEmpoloyeeComponent;
+  let fixture: ComponentFixture<NewEmpoloyeeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NewEmpoloyeeComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(NewEmpoloyeeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
